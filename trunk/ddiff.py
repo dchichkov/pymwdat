8dd0de9613f5bd5277f02bd38e17833b02068ea0
dchichkov <dchichkov@c8ac9f55-232d-5cd1-3ddd-d7820b40bd08>
2012-04-06 03:10:19 +0000

    *internal

--- trunk/ddiff.py
+++ trunk/ddiff.py
@@ -138,7 +138,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 """
 
-from ordereddict import OrderedDict
+from collections import OrderedDict
 
 def ddiff(a, b):
     ahi = len(a); bhi = len(b)          # find last matching 
